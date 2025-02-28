ffbb0ba2240bb37b1bb6e752f498f72c930b0903
RidgeBerry <crb0058@uah.edu>
2025-02-27 22:52:31 -0600

    This the commit for the homepage and the link bar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,25 @@
+<script>
+    import "../app.css"
+</script>
+
 <nav>
     <div style="margin-right: 20px;">
-        <a href="/">Home</a>
-        &nbsp&nbsp
+        <a href="/homepage">Home</a>
+        <a href=" ">Run Mode</a>
+        <a href=" ">Rosters</a>
+        <a href=" ">Past Games</a>
+        
         <a href="/players/all">View All Players</a>
-        &nbsp&nbsp
+       
         <a href="/players/add">Add a Player</a>
+
+       
+    </div>
+    <div class ="buttons">
+        <button type= "button">Register</button>
+        <button type= "button">Sign In</button>
     </div>
 </nav>
+
 
 <slot></slot>
---
+++ src/routes/homepage/+page.svelte
@@ -0,0 +1,51 @@
+
+
+<head>
+    <title>Home Page</title>
+    <style>
+        a:link {
+            text-decoration: none;
+            color: black;
+        }
+        a:visited {
+            text-decoration: none;
+            color: black;
+        }
+        a:hover {
+            color: gold;
+        }
+    </style>
+</head>
+<body>
+    <div>
+        <section class = "home-dash">
+            <h1>SMEGOL</h1>
+            <h3>Stats Made Easy, Game Operator's Log</h3>
+        </section>
+
+        <section class = "recent-games">
+            <h2>Recent Games</h2>
+            <a href=" ">
+                <div class = "game">
+                    <h3>Game 1</h3>
+                    <p>Score and Date</p>
+                </div>
+            </a>
+                
+            <a href=" ">
+                <div class = "game">
+                    <h3>Game 2</h3>
+                    <p>Score and Date</p>
+                </div>
+            </a>
+
+            <a href=" ">
+                <div class = "game">
+                    <h3>Game 2</h3>
+                    <p>Score and Date</p>
+                </div>
+            </a>
+        </section>
+    </div>
+
+</body>
