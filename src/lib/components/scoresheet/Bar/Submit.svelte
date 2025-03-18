06bc837225ba01aecb4ec6be485d622bb28d979b
RoboRobert <nathan.estep@gmail.com>
2025-03-18 15:19:01 -0500

    starting on the submit functionality

--- src/lib/components/scoresheet/Bar/Submit.svelte
+++ src/lib/components/scoresheet/Bar/Submit.svelte
@@ -1,9 +1,30 @@
 <script lang="ts">
   import { base } from "$app/paths";
+  import { json } from "@sveltejs/kit";
+  import { awayGoals, awayGoalTrack, clears, extraMan, faceoffs, groundBalls, homeGoals, homeGoalTrack, metaStats, players, shots, timeouts } from "../data.svelte";
+
+  function submitScoresheet() {
+    const scoresheetData = {
+      players: players,
+      homeGoals: homeGoals,
+      awayGoals: awayGoals,
+      homeGoalTrack: homeGoalTrack,
+      awayGoalTrack: awayGoalTrack,
+      groundBalls: groundBalls,
+      shots:shots,
+      clears: clears,
+      faceoffs: faceoffs,
+      extraMan: extraMan,
+      timeouts: timeouts,
+      metaStats: metaStats,
+    };
+    const scoresheetJSON = JSON.stringify(scoresheetData);
+    console.log(scoresheetJSON);
+  }
 </script>
 
 <div class="noBorder">
-  <button>SUBMIT SCORESHEET</button>
+  <button onclick={submitScoresheet}>SUBMIT SCORESHEET</button>
 </div>
 
 <style>
