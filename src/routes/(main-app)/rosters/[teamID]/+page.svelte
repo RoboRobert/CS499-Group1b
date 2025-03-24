60d515a171e71d187df42bce941c19121f25163d
RidgeBerry <crb0058@uah.edu>
2025-03-23 19:59:58 -0500

    Commit for some minor css changes

--- src/routes/(main-app)/rosters/[teamID]/+page.svelte
+++ src/routes/(main-app)/rosters/[teamID]/+page.svelte
@@ -223,8 +223,12 @@
     <h3>{currentTeam.coach}</h3>
 </section>
 <section class="list-section-1">
-    <h2>Players</h2>
+    <div class= "buttons">
     <button on:click={() => openEditModal(defaultPlayer)} type="button">Add Player</button>
+    </div>
+    <div>
+    <h2>Players</h2>
+    </div>
     <div>
     {#each currentTeam.players as player}
         <div class="game">
@@ -232,7 +236,7 @@
             <h3>{player.firstName}</h3>
             <p>{player.position}</p>
         </a>
-        <div class = "button-container">
+        <div class = "buttons">
         <button on:click={() => openEditModal(player)} type="button">Edit</button>
         <button on:click={() => handleDeletePlayer(player)} type="button">Delete</button>
         </div>
@@ -273,7 +277,59 @@
         </div>
         <div class="form-group">
         <label for="player-state">State:</label>
-        <input type="text" name="player-state" value={editingPlayer.state} >
+        <select name="state" value={editingPlayer.state}>
+            <option value="">Select a state</option>
+            <option value="AL">AL</option>
+            <option value="AK">AK</option>
+            <option value="AZ">AZ</option>
+            <option value="AR">AR</option>
+            <option value="CA">CA</option>
+            <option value="CO">CO</option>
+            <option value="CT">CT</option>
+            <option value="DE">DE</option>
+            <option value="FL">FL</option>
+            <option value="GA">GA</option>
+            <option value="HI">HI</option>
+            <option value="ID">ID</option>
+            <option value="IL">IL</option>
+            <option value="IN">IN</option>
+            <option value="IA">IA</option>
+            <option value="KS">KS</option>
+            <option value="KY">KY</option>
+            <option value="LA">LA</option>
+            <option value="ME">ME</option>
+            <option value="MD">MD</option>
+            <option value="MA">MA</option>
+            <option value="MI">MI</option>
+            <option value="MN">MN</option>
+            <option value="MS">MS</option>
+            <option value="MO">MO</option>
+            <option value="MT">MT</option>
+            <option value="NE">NE</option>
+            <option value="NV">NV</option>
+            <option value="NH">NH</option>
+            <option value="NJ">NJ</option>
+            <option value="NM">NM</option>
+            <option value="NY">NY</option>
+            <option value="NC">NC</option>
+            <option value="ND">ND</option>
+            <option value="OH">OH</option>
+            <option value="OK">OK</option>
+            <option value="OR">OR</option>
+            <option value="PA">PA</option>
+            <option value="RI">RI</option>
+            <option value="SC">SC</option>
+            <option value="SD">SD</option>
+            <option value="TN">TN</option>
+            <option value="TX">TX</option>
+            <option value="UT">UT</option>
+            <option value="VT">VT</option>
+            <option value="VA">VA</option>
+            <option value="WA">WA</option>
+            <option value="WV">WV</option>
+            <option value="WI">WI</option>
+            <option value="WY">WY</option>
+          </select>
         {#if errors.state}
             <p class="error">{errors.state}</p>
         {/if}
@@ -288,6 +344,7 @@
         <div class="form-group">
         <label for="player-position">Position:</label>
         <select name="player-position" value={editingPlayer.position} >
+            <option value="">Select a position</option>
             <option value="Attack">Attack</option>
             <option value="Midfield">Midfield</option>
             <option value="Defense">Defense</option>
@@ -302,6 +359,7 @@
         <div class="form-group">
         <label for="player-class">Class:</label>
         <select name="player-class" value={editingPlayer.class} >
+            <option value="">Select a class</option>
             <option value="Freshman">Freshman</option>
             <option value="Sophomore">Sophomore</option>
             <option value="Junior">Junior</option>
