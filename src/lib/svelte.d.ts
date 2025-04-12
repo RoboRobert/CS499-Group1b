5409b3fad568e935c67c9cca5a58fb482341d395
Meps320 <jamier.moses@hotmail.com>
2025-04-12 11:28:11 -0500

    pulled from main

--- src/lib/svelte.d.ts
+++ src/lib/svelte.d.ts
@@ -1,4 +1,5 @@
 // src/svelte.d.ts
+// check if project is up to date with main branch
 declare module '*.svelte' {
     import { SvelteComponentTyped } from 'svelte';
     export default class Component extends SvelteComponentTyped<any, any, any> {}
