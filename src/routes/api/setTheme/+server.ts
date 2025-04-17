a9006f120cf3696bbef62a25f25bc6e4d6966fa0
RoboRobert <nathan.estep@gmail.com>
2025-04-17 11:07:20 -0500

    fixes theme cookie. All the cookies seem to work now

--- src/routes/api/setTheme/+server.ts
+++ src/routes/api/setTheme/+server.ts
@@ -5,8 +5,11 @@ export async function GET({ url, cookies }) {
 
   if (theme) {
     cookies.set("colortheme", theme, {
-      path: "/",
-      maxAge: 60 * 60 * 24 * 365,
+      path: '/',  // The cookie is valid for all paths
+      sameSite: 'strict',  // Prevents the cookie from being sent on cross-site requests
+      httpOnly: true,  // For security, prevent access to the cookie via JavaScript
+      secure: process.env.NODE_ENV === 'production', // Set the secure flag in production
+      maxAge: 60 * 60 * 24,  // Cookie expires in 1 day (adjust as needed)
     });
   }
 
