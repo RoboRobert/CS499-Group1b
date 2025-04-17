347ce1dd8536ac6465c401bb0af64e54d33514f8
RoboRobert <nathan.estep@gmail.com>
2025-04-17 00:34:24 -0500

    fixes full game stats bug

--- src/lib/components/scoresheet/Bar/Halftime.svelte
+++ src/lib/components/scoresheet/Bar/Halftime.svelte
@@ -8,7 +8,7 @@
 
   function modifyTurnoverHalf2(event: any, side: number) {
     const value = Number(event.target.value);
-    if(value < turnovers[side].half1) {
+    if (value < turnovers[side].half1) {
       event.target.value = turnovers[side].half1;
       return;
     }
@@ -17,7 +17,7 @@
 
   function modifySubsHalf2(event: any, side: number) {
     const value = Number(event.target.value);
-    if(value < substitutions[side].half1) {
+    if (value < substitutions[side].half1) {
       event.target.value = substitutions[side].half1;
       return;
     }
@@ -60,11 +60,11 @@
     <h2>DISPLAY STATS</h2>
     <div class="radio-group">
       <div class="radio-item">
-        <input type="radio" id="option1" name="statsGroup" value="halftime" bind:group={statsMode} class="radioButton"/>
+        <input type="radio" id="option1" name="statsGroup" value="halftime" bind:group={statsMode} class="radioButton" />
         <label class="radioLabel" for="option1">HALFTIME</label>
       </div>
       <div class="radio-item">
-        <input type="radio" id="option2" name="statsGroup" value="fullgame" bind:group={statsMode} class="radioButton"/>
+        <input type="radio" id="option2" name="statsGroup" value="fullgame" bind:group={statsMode} class="radioButton" />
         <label class="radioLabel" for="option2">FULL GAME</label>
       </div>
     </div>
@@ -76,10 +76,10 @@
         Ground Balls: {teamName[0].split(" ")[0]}-{groundBalls[0][0] + groundBalls[0][1]}, {teamName[1].split(" ")[0]}-{groundBalls[1][0] + groundBalls[1][1]}
       </div>
       <div>
-        Faceoffs: {teamName[0].split(" ")[0]}-{faceoffs[0][0].won + faceoffs[0][1].won}, {teamName[1].split(" ")[0]}-{faceoffs[1][1].won + faceoffs[1][1].won}
+        Faceoffs: {teamName[0].split(" ")[0]}-{faceoffs[0][0].won + faceoffs[0][1].won}, {teamName[1].split(" ")[0]}-{faceoffs[1][0].won + faceoffs[1][1].won}
       </div>
       <div>
-        Clears: {teamName[0].split(" ")[0]}-{clears[0][0].won + clears[0][1].won}, {teamName[1].split(" ")[0]}-{clears[1][1].won + clears[1][1].won}
+        Clears: {teamName[0].split(" ")[0]}-{clears[0][0].won + clears[0][1].won}, {teamName[1].split(" ")[0]}-{clears[1][0].won + clears[1][1].won}
       </div>
       <div>
         Fouls: {teamName[0].split(" ")[0]}-{penalties[0].filter((p) => checkObj(p)).map((p) => p.quarter <= 2).length}, {teamName[1].split(" ")[0]}-{penalties[1]
@@ -92,56 +92,55 @@
           .reduce((prev, curr) => curr + prev)}
       </div>
       <div class="innerRow">
-        <p class="smallBox">Turnovers: </p>
-        <div class="smallBox"> {teamName[0].split(" ")[0]}-</div>
-        <input class="smallInput" min="0" max="99" autocomplete="off" type="number" bind:value={turnovers[0].half1}>
-        <div class="smallBox">, {teamName[1].split(" ")[0]}-, </div>
-        <input class="smallInput" min="0" max="99" autocomplete="off" type="number" bind:value={turnovers[1].half1}>
+        <p class="smallBox">Turnovers:</p>
+        <div class="smallBox">{teamName[0].split(" ")[0]}-</div>
+        <input class="smallInput" min="0" max="99" autocomplete="off" type="number" bind:value={turnovers[0].half1} />
+        <div class="smallBox">, {teamName[1].split(" ")[0]}-,</div>
+        <input class="smallInput" min="0" max="99" autocomplete="off" type="number" bind:value={turnovers[1].half1} />
       </div>
       <div class="innerRow">
-        <p class="smallBox">Turnovers: </p>
-        <div class="smallBox"> {teamName[0].split(" ")[0]}-</div>
-        <input class="smallInput" min="0" max="99" autocomplete="off" type="number" bind:value={substitutions[0].half1}>
-        <div class="smallBox">, {teamName[1].split(" ")[0]}-, </div>
-        <input class="smallInput" min="0" max="99" autocomplete="off" type="number" bind:value={substitutions[1].half1}>
+        <p class="smallBox">Turnovers:</p>
+        <div class="smallBox">{teamName[0].split(" ")[0]}-</div>
+        <input class="smallInput" min="0" max="99" autocomplete="off" type="number" bind:value={substitutions[0].half1} />
+        <div class="smallBox">, {teamName[1].split(" ")[0]}-,</div>
+        <input class="smallInput" min="0" max="99" autocomplete="off" type="number" bind:value={substitutions[1].half1} />
       </div>
     {/if}
     {#if statsMode !== "halftime"}
       <div>
-        Shots: {teamName[0].split(" ")[0]}-{shots[0][0] + shots[0][1]}, {teamName[1].split(" ")[0]}-{shots[1][0] + shots[1][1]}
+        Shots: {teamName[0].split(" ")[0]}-{shots[0].reduce((c, p) => c + p)}, {teamName[1].split(" ")[0]}-{shots[1].reduce((c, p) => c + p)}
       </div>
       <div>
-        Ground Balls: {teamName[0].split(" ")[0]}-{groundBalls[0][0] + groundBalls[0][1]}, {teamName[1].split(" ")[0]}-{groundBalls[1][0] + groundBalls[1][1]}
+        Ground Balls: {teamName[0].split(" ")[0]}-{groundBalls[0].reduce((c, p) => c + p)}, {teamName[1].split(" ")[0]}-{groundBalls[1].reduce((c, p) => c + p)}
       </div>
       <div>
-        Faceoffs: {teamName[0].split(" ")[0]}-{faceoffs[0][0].won + faceoffs[0][1].won}, {teamName[1].split(" ")[0]}-{faceoffs[1][1].won + faceoffs[1][1].won}
+        Faceoffs: {teamName[0].split(" ")[0]}-{faceoffs[0].map((f) => f.won).reduce((c,p) => c + p)}, {teamName[1].split(" ")[0]}-{faceoffs[1].map((f) => f.won).reduce((c,p) => c + p)}
       </div>
       <div>
-        Clears: {teamName[0].split(" ")[0]}-{clears[0][0].won + clears[0][1].won}, {teamName[1].split(" ")[0]}-{clears[1][1].won + clears[1][1].won}
+        Clears: {teamName[0].split(" ")[0]}-{clears[0].map((f) => f.won).reduce((c,p ) => c + p)}, {teamName[1].split(" ")[0]}-{clears[1].map((f) => f.won).reduce((c,p) => c + p)}
       </div>
       <div>
-        Fouls: {teamName[0].split(" ")[0]}-{penalties[0].filter((p) => checkObj(p)).map((p) => p.quarter <= 2).length}, {teamName[1].split(" ")[0]}-{penalties[1]
-          .filter((p) => checkObj(p))
-          .map((p) => p.quarter <= 2).length}
+        Fouls: {teamName[0].split(" ")[0]}-{penalties[0].filter((p) => checkObj(p)).length}, {teamName[1].split(" ")[0]}-{penalties[1]
+          .filter((p) => checkObj(p)).length}
       </div>
       <div>
-        Saves: {teamName[0].split(" ")[0]}-{saves[0].map((save) => save.qtr1 + save.qtr2).reduce((prev, curr) => curr + prev)}, {teamName[1].split(" ")[0]}-{saves[1]
-          .map((save) => save.qtr1 + save.qtr2)
+        Saves: {teamName[0].split(" ")[0]}-{saves[0].map((save) => save.qtr1 + save.qtr2 + save.qtr3 + save.qtr4 + save.ot).reduce((prev, curr) => curr + prev)}, {teamName[1].split(" ")[0]}-{saves[1]
+          .map((save) => save.qtr1 + save.qtr2 + save.qtr3 + save.qtr4 + save.ot)
           .reduce((prev, curr) => curr + prev)}
       </div>
       <div class="innerRow">
-        <p class="smallBox">Turnovers: </p>
-        <div class="smallBox"> {teamName[0].split(" ")[0]}-</div>
-        <input class="smallInput" min="0" max="99" autocomplete="off" type="number" value={turnovers[0].half1 + turnovers[0].half2} onchange={(e) => modifyTurnoverHalf2(e, 0)}>
-        <div class="smallBox">, {teamName[1].split(" ")[0]}-, </div>
-        <input class="smallInput" min="0" max="99" autocomplete="off" type="number" value={turnovers[1].half1 + turnovers[1].half2} onchange={(e) => modifyTurnoverHalf2(e, 1)}>
+        <p class="smallBox">Turnovers:</p>
+        <div class="smallBox">{teamName[0].split(" ")[0]}-</div>
+        <input class="smallInput" min="0" max="99" autocomplete="off" type="number" value={turnovers[0].half1 + turnovers[0].half2} onchange={(e) => modifyTurnoverHalf2(e, 0)} />
+        <div class="smallBox">, {teamName[1].split(" ")[0]}-,</div>
+        <input class="smallInput" min="0" max="99" autocomplete="off" type="number" value={turnovers[1].half1 + turnovers[1].half2} onchange={(e) => modifyTurnoverHalf2(e, 1)} />
       </div>
       <div class="innerRow">
-        <p class="smallBox">Turnovers: </p>
-        <div class="smallBox"> {teamName[0].split(" ")[0]}-</div>
-        <input class="smallInput" min="0" max="99" autocomplete="off" type="number" value={substitutions[0].half1 + substitutions[0].half2} onchange={(e) => modifySubsHalf2(e, 0)}>
-        <div class="smallBox">, {teamName[1].split(" ")[0]}-, </div>
-        <input class="smallInput" min="0" max="99" autocomplete="off" type="number" value={substitutions[1].half1 + substitutions[1].half2} onchange={(e) => modifySubsHalf2(e, 1)}>
+        <p class="smallBox">Turnovers:</p>
+        <div class="smallBox">{teamName[0].split(" ")[0]}-</div>
+        <input class="smallInput" min="0" max="99" autocomplete="off" type="number" value={substitutions[0].half1 + substitutions[0].half2} onchange={(e) => modifySubsHalf2(e, 0)} />
+        <div class="smallBox">, {teamName[1].split(" ")[0]}-,</div>
+        <input class="smallInput" min="0" max="99" autocomplete="off" type="number" value={substitutions[1].half1 + substitutions[1].half2} onchange={(e) => modifySubsHalf2(e, 1)} />
       </div>
     {/if}
     <div class="modal-actions">
@@ -163,12 +162,12 @@
     min-height: 0;
     opacity: 100%;
     box-sizing: border-box;
-    height:100%;
-    flex:0.15;
+    height: 100%;
+    flex: 0.15;
   }
 
   .smallBox {
-    all:unset;
+    all: unset;
     text-align: center;
     min-width: 0;
     min-height: 0;
