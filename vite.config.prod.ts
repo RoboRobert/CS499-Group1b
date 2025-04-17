75478cf3fe3ffb7d145bc815889860830554c1bf
RoboRobert <nathan.estep@gmail.com>
2025-04-17 11:00:52 -0500

    splits into two config files, one for dev one for prod

---
+++ vite.config.prod.ts
@@ -0,0 +1,17 @@
+import { sveltekit } from "@sveltejs/kit/vite";
+import { defineConfig } from "vite";
+import dns from "node:dns";
+
+dns.setDefaultResultOrder("verbatim");
+
+export default defineConfig({
+  plugins: [sveltekit()],
+  server: {
+    strictPort: true,
+    port: 8080,
+    hmr: {
+      clientPort: 80,
+    },
+    allowedHosts: ["ec2-3-136-29-133.us-east-2.compute.amazonaws.com"],
+  },
+});
