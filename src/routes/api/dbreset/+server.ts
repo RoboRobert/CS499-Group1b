144c122ce27b341395c4b45bd5f4246f4a94a37d
RoboRobert <nathan.estep@gmail.com>
2025-04-27 10:01:44 -0500

    removes auth for dbreset so you can actually set up the app

--- src/routes/api/dbreset/+server.ts
+++ src/routes/api/dbreset/+server.ts
@@ -1,11 +1,7 @@
-import { error, json } from '@sveltejs/kit';
 import { dbReset } from '$lib/database/dbreset';
+import { json } from '@sveltejs/kit';
 
 export async function GET({cookies}) {
-    const token = cookies.get("user-role");
-    if (token !== "admin") {
-      error(403, "You don't have the right O you don't have the right");
-    }
     await dbReset();
     return json({message: 'Database reset'});
 }
